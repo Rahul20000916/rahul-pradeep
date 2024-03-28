b9a917a3bf98c333bce413f174a4fa2f4e85832f
unknown <rahulpradeep061@gmail.com>
2024-03-28 10:17:46 +0530

    updated

---
+++ src/components/HomePage/AboutComponet.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutComponet = () => {
+  return (
+    <div class="py-2 px-2 h-full">
+      <div class="group relative block h-full bg-white before:absolute before:inset-0 before:rounded-md before:border-2 before:border-dashed before:border-gray-900">
+        <div class="rounded-md h-full border-2 border-gray-900 bg-white transition group-hover:-translate-y-2 ltr:group-hover:-translate-x-2 rtl:group-hover:translate-x-2">
+          <div class="p-4 sm:p-6 h-full">
+            <div class="flex items-start justify-between h-full">
+              <span aria-hidden="true" role="img" class="text-lg sm:text-xl">
+                🔢
+              </span>
+            </div>
+            <h2 class="mt-4 font-medium text-gray-900 sm:text-lg">About</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutComponet;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import AboutComponent from "../components/HomePage/AboutComponet";
+import SkillsComponent from "../components/HomePage/SkillsComponent";
+import PortfolioComponent from "../components/HomePage/PortfolioComponent";
+import ContactComponent from "../components/HomePage/ContactComponent";
 const HomePage = () => {
   return (
-    <div>
-      HomePage
+    <div className="h-full">
+      <AboutComponent />
+      <SkillsComponent />
+      <PortfolioComponent />
+      <ContactComponent />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
